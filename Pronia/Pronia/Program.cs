62046c16211a6cf6b226e0ffe8211bf087993958
Tebriz123 <necefovtebriz5555@gmail.com>
2025-12-05 18:00:48 +0400

    Login and Register

--- Pronia/Pronia/Program.cs
+++ Pronia/Pronia/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Pronia.DAL;
+using Pronia.Models;
 
 
 namespace Pronia
@@ -17,12 +19,29 @@ namespace Pronia
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
             });
 
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
+            {
+                opt.Password.RequiredLength = 8;
+                opt.Password.RequireNonAlphanumeric = false;
+
+                opt.User.RequireUniqueEmail = false;
+                opt.Lockout.MaxFailedAccessAttempts = 3;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3);
+
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
             //builder.Services.AddScoped<IEmailService, EmailService>();
 
             //builder.Services.AddSingleton<EmailService>();
             //builder.Services.AddTransient<EmailService>();
 
             var app = builder.Build();
+
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.MapControllerRoute(
                 "default",
                 "{area:exists}/{controller=home}/{action=index}/{id?}"
